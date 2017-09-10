88d6b501c81ccee9bc5621fa8be903374522e7f5
Kiran <Kiran>
2017-09-10 15:09:06 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const hbs=require('hbs');
+var app=express();
+hbs.registerPartials(__dirname+"/views/partials");
+app.set("view engiine",'hbs');
+app.use(express.static(__dirname+"/public"));
+app.get('/',(req,res)=>{
+  // res.send("<h1>hello express!!!!</h1>")
+//  res.send({
+//    name:"kiran",
+//    likes:[
+//       'foot',
+//       'crick'
+//
+//    ]
+// });
+
+res.render('home.hbs',{
+   mes:"myt hhooooooooooome",
+    pagetitle:"my page dynamic",
+    year:new Date().getFullYear()
+
+});
+});
+
+app.get('/about',(req,res)=>{
+
+res.render('about.hbs',{
+    pagetitle:"my page dynamic",
+    year:new Date().getFullYear()
+
+});
+
+});
+
+
+app.get('/error',(req,res)=>{
+
+  // res.send(",<h1>eroor le mangya<h1>");
+res.send({
+     error:"error le mangya",
+     status:"not bad"
+});
+});
+app.listen(3000);
